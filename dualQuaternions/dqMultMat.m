12daa07162ec2d8c127c0317b1333f45d373b13c
Yasuhiro Yoshimura <yoshimura@aero.kyushu-u.ac.jp>
2025-06-20 10:53:23 +0900

    transformed .mlx to m

--- dualQuaternions/dqMultMat.m
+++ dualQuaternions/dqMultMat.m
@@ -1,4 +1,4 @@
-%[text] # dual quaternion multiplication
+%[text] # dual quaternion multiplication matrix
 %[text] `scalar:` specify the definition of the quaternion 
 %[text] `scalar == 0`
 %[text] ${\\bf q} = \[q\_0,q\_1,q\_2,q\_3\]^T=\[\\cos(\\theta/2), {\\bf e}^T\\sin(\\theta/2)\]^T$
@@ -6,7 +6,7 @@
 %[text] ${\\bf q} = \[q\_1,q\_2,q\_3, q\_4\]^T=\[{\\bf e}^T\\sin(\\theta/2), \\cos(\\theta/2)\]^T$
 %[text] `def`: specifies the definition of the quaternion multiplication
 %[text] `def == 0`
-%[text] for dual quaternion $\\tilde{\\bf q\n}$ and $\\tilde{\\bf p}\n$
+%[text] for dual quaternion $\\tilde{\\bf q\n}$
 %[text] $\\tilde{\\bf q} \\odot \\tilde{\\bf p}= \\left\[\\begin{array}{cc}\n{\\bf q}\_r\\odot & 0\_{4\\times4}     \\\\\n{\\bf q}\_d\\odot & {\\bf q}\_r\\odot    \\\\\n\\end{array}\\right\]$
 %[text] where
 %[text] ${\\bf q}\\odot{\\bf p}= \\left\[\\begin{array}{cc}\n     q\_{4}\\bar{{\\bf{p}}}+p\_{4}\\bar{{\\bf{q}}} + \\bar{{\\bf{q}}}\\times\\bar{{\\bf{p}}}\\\\\nq\_{4}p\_{4}-\\bar{{\\bf{q}}}^{T}\\bar{{\\bf{p}}}\n\\end{array}\\right\]$
@@ -14,31 +14,29 @@
 %[text] $\\tilde{\\bf q} \\otimes \\tilde{\\bf p}= \\left\[\\begin{array}{cc}\n{\\bf q}\_r\\otimes & 0\_{4\\times4}     \\\\\n{\\bf q}\_d\\otimes & {\\bf q}\_r\\otimes    \\\\\n\\end{array}\\right\]$
 %[text] where
 %[text] ${\\bf q}\\otimes{\\bf p}= \\left\[\\begin{array}{cc}\n     q\_{4}\\bar{{\\bf{p}}}+p\_{4}\\bar{{\\bf{q}}} - \\bar{{\\bf{q}}}\\times\\bar{{\\bf{p}}}\\\\\nq\_{4}p\_{4}-\\bar{{\\bf{q}}}^{T}\\bar{{\\bf{p}}}\n\\end{array}\\right\]$
-%[text] dq: dual quaternion \[real, dual\], nx8
-%[text] dp: dual quaternion \[real, dual\], nx8
+%[text] dqM: dual quaternion matrix, 8x8
 %[text] ## note
 %[text] NA
 %[text] ## references 
 %[text] NA
 %[text] ## revisions
-%[text] 20231219  y.yoshimura
-%[text] See also qMult.
-function dq = dqMult_(scalar, def, dq, dp)
+%[text] 20240109  y.yoshimura
+%[text] See also qMult, qMultMat, dqMult.
+function dqM = dqMultMat(scalar, def, dq)
+% arguments
+%     scalar (1,1) {mustBeMember(scalar, [0, 4])}
+%     def (1,1) {mustBeMember(def, [0, 1])}
+%     dq (:,8) {mustBeNumeric}
+% end
 
 dqReal = dq(:,1:4);
 dqDual = dq(:,5:8);
 
-dpReal = dp(:,1:4);
-dpDual = dp(:,5:8);
-
-realPart = qMult_(scalar, def, dqReal, dpReal);
-dualPart = qMult_(scalar, def, dqReal, dpDual) + qMult_(scalar, def, dqDual, dpReal);
-
-dq = [realPart dualPart];
+dqM = [qMultMat(scalar, def, dqReal) zeros(4,4)
+    qMultMat(scalar, def, dqDual) qMultMat(scalar, def, dqReal)];
 
 
 end
-
 
 %[appendix]{"version":"1.0"}
 %---
--- dualQuaternions/sclerp.m
+++ dualQuaternions/sclerp.m
@@ -1,46 +1,91 @@
+%[text] # Screw linear interpolation of dual quaternions (sclerp)
+%[text] `t`: normalzied time, i.e., $0\<t\\leq 1$
+%[text] `scalar:` specify the definition of the quaternion 
+%[text] `scalar == 0`
+%[text] ${\\bf q} = \[q\_0,q\_1,q\_2,q\_3\]^T=\[\\cos(\\theta/2), {\\bf e}^T\\sin(\\theta/2)\]^T$
+%[text] `scalar == 4`
+%[text] ${\\bf q} = \[q\_1,q\_2,q\_3, q\_4\]^T=\[{\\bf e}^T\\sin(\\theta/2), \\cos(\\theta/2)\]^T$
+%[text] `dq1`: starting dual quaternion, 1x8 
+%[text] `dq2`: ending dual quaternion, 1x8 
+%[text] dqt: interpolated dual quaternions, nx8
 function dqt = sclerp(t, scalar, dq1, dq2)
-% ----------------------------------------------------------------------
-%   SCrew Linear Interpolation of quaternions (slerp)
-%    20210223  y.yoshimura
-%    Inputs: t: normalzied time, i.e., 0<=t<=1
-%            scalar: specifies the definition of the quaternion
-%            scalar == 0,  q0:= cos(theta/2), q = [q0, q1, q2, q3]
-%            scalar == 4,  q4:= cos(theta/2), q = [q1, q2, q3, q4]         
-%            dq1: starting dual quaternion, 1x8
-%            dq2: ending dual quaternion, 1x8
-%   Outputs: qt: interpolated quaternions, Nx8
-%   related function files:
-%   note:
-%   cf:
-%   revisions;
-%   function qt = sclerp(t, scalar, dq1, dq2)
-%   (c) 2021 yasuhiro yoshimura
-%----------------------------------------------------------------------
+% arguments
+%     t (:,1) {mustBeNumeric}
+%     scalar (1,1) {mustBeMember(scalar, [0, 4])}
+%     dq1 (:,8) {mustBeNumeric}
+%     dq2 (:,8) {mustBeNumeric}
+% end
 
 t = t(:); % column vector
 
-% rotation
-dqtmp = dqMult(scalar, 0, dqInv(scalar,dq1), dq2);
+nT = length(t);
 
-eTheta = (scalar == 0) .* acos(dqtmp(1)) ...
-    + (scalar == 4) .* acos(dqtmp(4));  % Euler angle of rotation
-eTheta = eTheta * 2;
-eAxis = (scalar == 0) .* dqtmp(2:4) ...  % Euler axis of rotation
-    + (scalar == 4) .* dqtmp(1:3);
-eAxis = eAxis ./ sin(eTheta/2);
+%[text] ## rotation
+dqtmp = dqMult(scalar, 0, dqInv(scalar, dq1), dq2);
 
-% translation
-d = (scalar == 0) .* dqtmp(5) ...
-    + (scalar == 4) .* dqtmp(8);
-d = -2 .* d ./ sin(eTheta/2);
+% scalarに基づく分岐を最初に行い、重複計算を避ける
+if scalar == 0
+    eTheta = 2 * acos(dqtmp(1));
+    eAxis = dqtmp(2:4);
+    d = dqtmp(5);
+    dqVec = dqtmp(6:8);
+else % scalar == 4
+    eTheta = 2 * acos(dqtmp(4));
+    eAxis = dqtmp(1:3);
+    d = dqtmp(8);
+    dqVec = dqtmp(5:7);
+end
 
-dqVec = (scalar == 0) .* dqtmp(6:8) ...
-    + (scalar == 4) .* dqtmp(5:7);
-p = (dqVec - d ./ 2 .* cos(eTheta/2) .* eAxis) ./ sin(eTheta/2);
+% 共通計算を事前に行う
+sinHalfTheta = sin(eTheta/2);
+cosHalfTheta = cos(eTheta/2);
 
-dq1tmp = kron(ones(size(t,1),1), dq1);
+% 0除算チェック
+if abs(sinHalfTheta) < eps
+    % 回転がない場合の処理
+    dqt = repmat(dq1, nT, 1);
+    return;
+end
 
-dqt = (scalar == 0) .* dqMult(scalar, 0, dq1tmp, [cos(t.*eTheta/2), eAxis.*sin(t.*eTheta./2), -t.*d./2.*sin(t.*eTheta./2), p.*sin(t.*eTheta./2)+t.*d./2.*cos(t.*eTheta./2).*eAxis]) ...
-    + (scalar == 4) .* dqMult(scalar, 0, dq1tmp, [eAxis.*sin(t.*eTheta./2), cos(t.*eTheta/2), p.*sin(t.*eTheta./2)+t.*d./2.*cos(t.*eTheta./2).*eAxis, -t.*d./2.*sin(t.*eTheta./2)]);
+eAxis = eAxis / sinHalfTheta;
+
+d = -2 * d / sinHalfTheta;
+p = (dqVec - d/2 * cosHalfTheta * eAxis) / sinHalfTheta;
+
+% ベクトル化された三角関数計算
+tThetaHalf = t * eTheta/2;
+cosTthetaHalf = cos(tThetaHalf);
+sinTthetaHalf = sin(tThetaHalf);
+
+% 事前に繰り返される値を計算
+tdHalf = t * d/2;
+eAxisRep = repmat(eAxis', nT, 1);
+pRep = repmat(p', nT, 1);
+
+% 三角関数項を事前計算
+sinTerm = sinTthetaHalf;
+cosTerm = cosTthetaHalf;
+neg_t_d_half_sin = -tdHalf .* sinTerm;
+t_d_half_cos_eAxis = tdHalf .* cosTerm .* eAxisRep;
+
+% 最終的な dual quaternion の構築
+if scalar == 0
+    dqInterp = [cosTerm, ...
+                eAxisRep .* sinTerm, ...
+                neg_t_d_half_sin, ...
+                pRep .* sinTerm + t_d_half_cos_eAxis];
+else % scalar == 4
+    dqInterp = [eAxisRep .* sinTerm, ...
+                cosTerm, ...
+                pRep .* sinTerm + t_d_half_cos_eAxis, ...
+                neg_t_d_half_sin];
+end
+
+% 最終的な乗算
+dq1rep = repmat(dq1, nT, 1);
+dqt = dqMult(scalar, 0, dq1rep, dqInterp);
 
 end
+
+%[appendix]{"version":"1.0"}
+%---
