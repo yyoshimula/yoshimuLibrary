c8203a8b700f215e7f2cef04a0a181d3d661ac6d
Yasuhiro Yoshimura <yoshimura@aero.kyushu-u.ac.jp>
2025-06-23 12:31:12 +0900

    bug fixed

--- utility/gFigs.m
+++ utility/gFigs.m
@@ -6,57 +6,49 @@
 %[text] ## references 
 %[text] NA
 %[text] ## revisions
-%[text] 20240725  y.yoshimura, y.yoshimula@gmail.com
+%[text] 20250623  y.yoshimura, y.yoshimula@gmail.com
 %[text] See also .
 function gFigs(nDisp)
+% gFigs  ― 既存の Figure を選んだディスプレイ上にグリッド配置する
+%
+%  gFigs()          : プライマリディスプレイ（1）へ配置
+%  gFigs(nDisp)     : nDisp で指定したディスプレイへ配置（1,2,…）
+%
+%  2025-07-25  y.yoshimura
+%
 arguments
-    nDisp (1,1) {mustBeNumeric} = 1
-end
-wMargin = -50;              % width Margin
-hMargin = -100;               % height Margin
-
-% javaを使って各ディスプレイのサイズを取得
-screens = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
-
-% 指定されたディスプレイのサイズと位置を取得
-if nDisp > length(screens)
-    error('指定されたディスプレイ番号が存在しません。');
+    nDisp (1,1) {mustBeInteger,mustBePositive} = 1
 end
 
-% diplayサイズと位置を取得
-bounds = screens(nDisp).getDefaultConfiguration().getBounds();
-screenWidth = bounds.getWidth();
-screenHeight = bounds.getHeight();
-screenX = bounds.getX();
-screenY = bounds.getY();
+%%--- ディスプレイ情報
+monPos = get(0,'MonitorPositions');   % [left bottom width height]（pixel）
+if nDisp > size(monPos,1)
+    error('指定されたディスプレイ番号が存在しません．');
+end
+scr = monPos(nDisp,:);                % 対象ディスプレイ
 
-figHandles = findobj('Type', 'figure');
-nFigs = length(figHandles);
+%%--- Figure ハンドル取得
+figs = findall(0,'Type','figure','Visible','on'); % Visible な Figure
+nF   = numel(figs);
+if nF == 0,  warning('表示中の Figure がありません．');  return;  end
 
-% Define the number of rows and columns
-numRows = ceil(sqrt(nFigs));
-numCols = ceil(nFigs / numRows);
+%%--- グリッドサイズ計算
+nRow = ceil(sqrt(nF));
+nCol = ceil(nF/nRow);
 
-% Calculate figure width and height
-figureWidth = screenWidth / numCols;
-figureHeight = screenHeight / numRows;
+gap   = 50;                                   % マージン（ピクセル）
+fW    = (scr(3) - gap*(nCol+1)) / nCol;       % 幅
+fH    = (scr(4) - gap*(nRow+1)) / nRow;       % 高さ
 
+%%--- 各 Figure を移動
+for k = 1:nF
+    r = ceil(k/nCol);              % 行（1-based）
+    c = k - (r-1)*nCol;            % 列
 
-% Create and position each figure
-for i = 1:nFigs
+    left   = scr(1) + gap + (c-1)*(fW+gap);
+    bottom = scr(2) + scr(4) - r*(fH+gap);
 
-    % 行と列を計算
-    row = ceil(i / numCols);
-    col = mod(i-1, numCols) + 1;
-
-    % 位置を計算
-    left = screenX + (col-1) * figureWidth;
-    bottom = screenY + screenHeight - row * figureHeight;
-
-    % Figureの位置を設定
-    set(figure(i), 'Position', [left, bottom, wMargin+figureWidth, hMargin+figureHeight]);
-
-
+    set(figs(k), 'Units','pixels', 'Position',[left bottom fW fH]);
 end
 end
 
