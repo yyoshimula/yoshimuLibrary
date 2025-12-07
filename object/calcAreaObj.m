c81800aadc403aca26fa3b07807e454f81800924
Yasuhiro Yoshimura <y.yoshimura.a64@m.kyushu-u.ac.jp>
2025-12-07 21:31:11 +0900

    bug fix

--- object/calcAreaObj.m
+++ object/calcAreaObj.m
@@ -24,10 +24,9 @@ area = zeros(size(sat.faces, 1), 1);
 pos = zeros(size(sat.faces, 1), 3);
 
 for i = 1:size(sat.faces, 1)
-
     if size(sat.faces(i,:), 2) == 3 | isnan(sat.faces(i,4))
         nPolygon = 3;
-    elseif size(sat.faces(i,:), 2) == ~isnan(sat.faces(i,4))
+    elseif size(sat.faces(i,:), 2) == 4 | ~isnan(sat.faces(i,4))
         nPolygon = 4;
     end
 
