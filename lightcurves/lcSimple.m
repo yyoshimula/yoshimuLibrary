b2cc7d562fa69e362b35f1b7ce03c08ab8732d39
Yasuhiro Yoshimura <y.yoshimura.a64@m.kyushu-u.ac.jp>
2025-12-08 14:45:59 +0900

    refactor: remove old BRDF functions and update lightcurve function interfaces and documentation.

--- lightcurves/lcSimple.m
+++ lightcurves/lcSimple.m
@@ -1,26 +1,24 @@
+%[text] # calculating Lambertian diffusion and Perfect Speuclarity
+%[text] ## inputs
+%[text] `sat`: satellite configuration read with `readSC`
+%[text] `sunB:` sun vector from satellite to Sun expressed with body-fixed frame, Mx3 matrix
+%[text] `obsB:` observer from satellite with body-fixed frame, Mx3 matrix
+%[text] thr: threshold for speuclar lobe, rad
+%[text] ## outputs
+%[text] fObs: total reflectance BRDF, nFacet x M vector
+%[text] cd: diffuse part of BRDF, nFacet x M vector
+%[text] cs: specular part of BRDF, nFacet x M vector
+%[text] D: normal distribution function of BRDF, nFacet x M vector
+%[text] ## note
+%[text] NA
+%[text] ## references
+%[text] NA
+%[text] ## revisions
+%[text] 20251208  y.yoshimura, y.yoshimula@gmail.com
+%[text] See also lcAS, lcCT, readSC.
 function [sat, cd, cs] = lcSimple(sat, sunB, obsB, thr)
-% ----------------------------------------------------------------------
-%   calculate Light curves with Lambertian and Perfect Specular model
-%    20200909  y.yoshimura
-%    Inputs: sat, satellite shape definition by obj files, N facets
-%            sunB, sun vector from satellite to sun, unit vector, Mx3 matrix 
-%            obsB, observer vector from spacecraft to observer, unit vetor, Mx3 
-%            thr(optional): perfect specular threshold, rad
-%   Outputs: sat: sat.fObs is added, NxM matrix
-%   related function files:
-%   note:
-%   cf:
-%   revisions;
-% function sat = simpleBRDF(sat, sun_b, obs_b, thr)
-%   (c) 2022 yasuhiro yoshimura
-%----------------------------------------------------------------------
-switch nargin %入力引数の数で場合わけ
-    case 3
-        thr = deg2rad(1); % perfect specular threshold
-    case 4
-        thr = thr;
-    otherwise
-        error('invalid number of arguments')
+if nargin < 4
+    thr = deg2rad(1); % perfect specular threshold
 end
 sunB = sunB ./ vecnorm(sunB, 2, 2); %一応 normalize
 obsB = obsB ./ vecnorm(obsB, 2, 2);
@@ -28,17 +26,20 @@ obsB = obsB ./ vecnorm(obsB, 2, 2);
 h = sunB + obsB; % bisector vector of sun and observer vectors
 h = h ./ vecnorm(h, 2, 2); % Mx3 matrix
 
-NS = sat.normal * sunB'; % NxM
-NV = sat.normal * obsB'; % NxM
+% sat.normal * sunB'は，faceの数 N x 時間履歴の数 Mのmatrixになる
+NS = sat.normal * sunB'; % nFacet x M matrix
+NV = sat.normal * obsB'; % nFacet x M matrix
 
-% sat.normal * h'は，faceの数 N x 時間履歴の数 Mのmatrixになる
 cTotal = sat.Cd./pi ...
-    + 2.0 .* sat.Cs .* ((sat.normal * h') >= cos(thr)); 
-tmp = cTotal .* sat.area .* (sat.normal * sunB') .* (sat.normal * obsB'); 
+    + 2.0 .* sat.Cs .* ((sat.normal * h') >= cos(thr));
+sat.fObs = cTotal .* sat.area .* NS .* NV;
 
 % if the faces can be seen or not
 cd = sat.Cd ./ pi .* (NS > 0) .* (NV > 0);
-cs = 2.0 .* sat.Cs .* ((sat.normal * h') >= cos(thr)) .* (NS > 0) .* (NV > 0); 
-sat.fObs = tmp .* (NS > 0) .* (NV > 0); % NxM
+cs = 2.0 .* sat.Cs .* ((sat.normal * h') >= cos(thr)) .* (NS > 0) .* (NV > 0);
+sat.fObs = sat.fObs .* (NS > 0) .* (NV > 0); % nFacet x M matrix
 
 end
+
+%[appendix]{"version":"1.0"}
+%---
